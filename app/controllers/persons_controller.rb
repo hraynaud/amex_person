a837d96b44246795bef71c04a68923399243f8b0
hraynaud <herby.raynaud@gmail.com>
2017-08-06 15:12:54 -0400

    Cleans up person controller

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,16 +1,10 @@
 class PersonsController < ApplicationController
-
   def create
     person = Person.create(params.require(:person).permit(:name, :email, :date_of_birth))
     if person.valid?
       head :ok
     else
-      render json: {error: "Unable to create this person", list: person.errors.messages}
+      render json: {errors: person.errors.messages}, status: 422
     end
   end
-
-  def show
-
-  end
-
 end
